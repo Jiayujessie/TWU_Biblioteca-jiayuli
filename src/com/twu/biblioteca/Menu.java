6945846d27cceeeab939dde7ea3793ca6f2b5a5f
Jiayujessie <Jiayujessie@users.noreply.github.com>
2020-04-23 03:56:56 +0800

    (1.10) (1.11)(1.12)Return book and get messages

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca;
 import java.util.Scanner;
 
 public class Menu {
+    Borrow borrow = new Borrow();
 
     public static void main(String[] args) {
 
@@ -20,7 +21,7 @@ public class Menu {
 
     }
 
-}
+
 
     public void showMenu() {
         System.out.println("Please type the number：");//输入功能编号进入相应的功能
@@ -58,9 +59,46 @@ public class Menu {
     }
 
     // 图书信息展示功能
-    public void showBook() {
-        borrow.showBook();
+    public void showBook() { borrow.showBook();}
+
+    // 还书功能
+    public void returnBook() {
+        System.out.print("Please type the name of book you want to return：");//输入要还的书籍名称
+        Scanner scanner = new Scanner(System.in);
+        String bookname = scanner.nextLine();
+        int flag = borrow.testBook(bookname);
+        if (flag == 0) {
+            System.out.println(bookname + " not be borrowed.");
+            System.out.print("type 0 and enter to return main menu, type others to continue return books.");//输入0回车返回至主菜单，输入其他键则继续还书
+            String a = scanner.nextLine();
+            if(a.equals("0")){
+                showMenu();
+            }else {
+                returnBook();
+            }
+        } else if (flag == -1) {
+            System.out.println(bookname + " did not exist in library!");//不存在该图书馆中
+            System.out.print("type 0 and enter to return main menu, type others to continue return books.");
+            String a = scanner.nextLine();
+            if(a.equals("0")){
+                showMenu();
+            }else {
+                returnBook();
+            }
+        } else if (flag == 1) {
+
+            System.out.println(borrow.getBorrowReader(bookname) + " success to return the book！");//成功还书
+            borrow.setBorrowReader(bookname);
+            System.out.print("type 0 and enter to return main menu, type others to continue return books.");
+            String a = scanner.nextLine();
+            if(a.equals("0")){
+                showMenu();
+            }else {
+                returnBook();
+            }
+        }
     }
+
 
 
     // 书籍借阅功能
@@ -69,21 +107,20 @@ public class Menu {
         Scanner scanner = new Scanner(System.in);
         String bookname = scanner.nextLine();
         int flag = borrow.testBook(bookname);
-        switch (flag) {
-            case 1://不存在此书的情况
-                System.out.println("借书失败，" + bookname + "不存在该图书馆中。");
-                System.out.print("输入0回车返回至主菜单，输入1回车则继续借书：");
-                int a1 = scanner.nextInt();
-                switch (a1) {
-                    case 0:
-                        showMenu();
-                        break;
+        if (flag == 1) {//不存在此书的情况
+            System.out.println("借书失败，" + bookname + "不存在该图书馆中。");
+            System.out.print("输入0回车返回至主菜单，输入1回车则继续借书：");
+            int a1 = scanner.nextInt();
+            switch (a1) {
+                case 0:
+                    showMenu();
+                    break;
 
-                    case 1:
-                        borrowBook();
-                        break;
+                case 1:
+                    borrowBook();
+                    break;
 
-                }
+            }
         }
     }
 }
