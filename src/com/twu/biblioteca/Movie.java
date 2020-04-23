5c724e7f45001022fee32dbebec5a4f1904334d3
Jiayujessie <Jiayujessie@users.noreply.github.com>
2020-04-23 14:58:19 +0800

    2.1 / View a list of available movies

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,61 @@
+package com.twu.biblioteca;
+
+public class Movie {
+
+    public Movie() {
+        {
+
+            {
+                int i = 0;
+                while (i < 100) {
+
+                    i++;
+                }
+            }
+            for (int i = 0; i < 5; i++) {
+                switch (i) {
+                    case 0:
+                        moviename[i] = "movie 1";
+                        director[i] = "director 1";
+                        releasedate[i] = "2020-01-01";
+                        rate[i] = "111";
+                        break;
+                    case 1:
+                        moviename[i] = "movie 2";
+                        director[i] = "director 2";
+                        releasedate[i] = "2020-02-02";
+                        rate[i] = "222";
+                        break;
+                    case 2:
+                        moviename[i] = "movie 3";
+                        director[i] = "director 3";
+                        releasedate[i] = "2020-03-03";
+                        rate[i] = "333";
+                        break;
+                    case 3:
+                        moviename[i] = "movie 4 ";
+                        director[i] = "director 4";
+                        releasedate[i] = "2020-04-04";
+                        rate[i] = "444";
+                        break;
+                    case 4:
+                        moviename[i] = "movie 5";
+                        director[i] = "director 5";
+                        releasedate[i] = "2020-05-05";
+                        rate[i] = "555";
+                        break;
+                }
+            }
+
+        }
+    }
+
+    public String[] moviename = new String[10];// 书名
+    public String[] director = new String[10];// 作者
+    public String[] releasedate = new String[10];// 日期
+    public String[] rate = new String[10];// 总页数
+
+
+}
+
+
